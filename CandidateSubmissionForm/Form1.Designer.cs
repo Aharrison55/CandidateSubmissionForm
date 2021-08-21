0a6eabfc5101783009a390ff4652ec5729c73060
Aharrison55 <54919433+Aharrison55@users.noreply.github.com>
2021-08-21 11:44:39 -0500

    Add project files.

---
+++ CandidateSubmissionForm/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace CandidateSubmissionForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(47, 57);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(166, 22);
+            this.fname.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(47, 121);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(166, 22);
+            this.lname.TabIndex = 4;
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(90, 190);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(59, 22);
+            this.age.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Age";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 274);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CandidateSubmissionForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CandidateSubmissionForm
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=Harrison55!");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string insertQuery = "INSERT INTO votingdata.votinginfo(fname,lname,age) VALUES('" + fname.Text + "','" + lname.Text + "'," + age.Text + ")"; 
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(insertQuery, connection);
+            try
+            {
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("worked");
+
+                }
+                else
+                {
+                    MessageBox.Show("no work");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+           
+        }
+    }
+}
